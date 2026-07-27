5d19a35dc9b8e0e378e6630b8bb337e6bdf97fb7
vishalmaurya5 <vishalverma5359@gmail.com>
2026-07-27 09:18:26 +0530

    vercel chnage

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: __dirname,
+    root: path.join(__dirname, ".."),
   },
 };
 
